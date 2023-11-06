3c7cf00e8b8c6b57d86a6c623c844e9629aa758d
Dhyanms <dhyanms@gmail.com>
2023-11-06 13:02:59 +0530

    done

---
+++ y.py
@@ -0,0 +1,16 @@
+"""
+This Python module calculates the sum of two numbers and prints the result.
+"""
+
+# Define two variables and assign values to them
+A = 1
+B = 2
+C = 3
+
+# Calculate the sum of 'A' and 'B'
+RES = A + B
+RES1 = RES + C
+
+# Print the result
+print(RES)
+
